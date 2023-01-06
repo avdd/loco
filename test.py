23d46b81fffa65293af3a91cc3b19e7f814292fe
Adrian Dries <i@avdd.co>
2023-01-07 10:50:35 +1100

    failing test: redirect to home

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ opts = webdriver.FirefoxOptions()
 opts.headless = True
 browser = webdriver.Firefox(options=opts)
 browser.get('http://localhost:8000/')
+
+assert browser.current_url == 'http://localhost:8000/home'
+
 p = browser.find_element('tag name', 'p')
 assert p.text == 'Hello, world!'
 
