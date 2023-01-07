aff13dd88827ffa5d6ee56f49c4ac75250806b35
Adrian Dries <i@avdd.co>
2023-01-07 20:02:08 +1100

    use unittest

--- test.py
+++ test.py
@@ -1,21 +1,50 @@
 import os
 import sys
 import subprocess
+import unittest
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.support.wait import WebDriverWait
 
-path = os.path.dirname(__file__)
-main = os.path.join(path, 'main.py')
 
-with subprocess.Popen([sys.executable, main]) as server:
+class Test(unittest.TestCase):
 
-    opts = webdriver.FirefoxOptions()
-    opts.headless = True
-    with webdriver.Firefox(options=opts) as browser:
+    def start_server(self):
+        main = os.path.join(os.path.dirname(__file__), 'main.py')
+        # pylint: disable=consider-using-with
+        server = subprocess.Popen([sys.executable, main])
+        self.enterContext(server)
+        self.addCleanup(server.terminate)
+
+    def start_browser(self):
+        opts = webdriver.FirefoxOptions()
+        opts.headless = True
+        browser = webdriver.Firefox(options=opts)
+        self.addCleanup(browser.quit)
         browser.get('http://localhost:8000/')
+        self.browser = browser
 
-        assert browser.current_url == 'http://localhost:8000/home'
+    def get_root(self):
+        return self.browser.find_element('id', 'AppRoot')
 
-        p = browser.find_element('tag name', 'p')
-        assert p.text == 'Hello, world!'
+    def setUp(self):
+        self.start_server()
+        self.start_browser()
 
-    server.terminate()
+    def test_redirects_to_home(self):
+        self.assertEqual(self.browser.current_url,
+                         'http://localhost:8000/home')
+
+    @unittest.skip('todo')
+    def test_skeleton_html(self):
+        self.assertRaises(NoSuchElementException, self.get_root)
+
+        # browser.execute_script('StartLoadingForTesting()')
+        # wait = WebDriverWait(browser, timeout=5)
+
+        # p = browser.find_element('tag name', 'p')
+        # assert p.text == 'Hello, world!'
+
+
+if __name__ == '__main__':
+    unittest.main()
