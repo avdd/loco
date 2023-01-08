27299e58541ea88d86b884a97110ffa4dc9acb41
Adrian Dries <i@avdd.co>
2023-01-08 12:07:08 +1100

    refactor to separate html, js files

---
+++ app.js
@@ -0,0 +1,4 @@
+const p = document.createElement('p');
+p.textContent = 'Hello, world!'
+p.className = 'Home';
+document.body.appendChild(p);
--- main.py
+++ main.py
@@ -1,34 +1,39 @@
+import time
+import os
 from werkzeug.wrappers import Request, Response
 from werkzeug.serving import run_simple
+from werkzeug.utils import send_file
+
+
+HERE = os.path.dirname(__file__)
+
+
+def relpath(name):
+    return os.path.join(HERE, name)
+
+
+def readfile(name):
+    path = relpath(name)
+    return open(path, encoding='UTF8').read()
+
+
+def sendfile(request, name, ctype):
+    path = relpath(name)
+    return send_file(path, request.environ, mimetype=ctype)
+
+
+Request.sendfile = sendfile
 
 
 @Request.application
 def hello(rq: Request):  # pylint: disable=invalid-name
     if rq.path == '/':
         return Response(status=302, headers={'Location': '/home'})
-    if rq.path == '/app.js':
-        script = '''
-        const p = document.createElement('p');
-        p.textContent = 'Hello, world!'
-        p.className = 'Home';
-        document.body.appendChild(p);
-        '''
-        return Response(script, mimetype='text/javascript')
     if rq.path == '/home':
-        html = '''
-        <script>
-        function StartLoading() {
-            setTimeout(AddScript, 100);
-        }
-        function AddScript() {
-            const script = document.createElement('script');
-            script.type = 'module';
-            script.src = './app.js';
-            document.body.appendChild(script);
-        }
-        </script>
-        '''
-        return Response(html, mimetype='text/html')
+        return rq.sendfile('skeleton.html', 'text/html')
+    if rq.path == '/app.js':
+        time.sleep(1)
+        return rq.sendfile('app.js', 'text/javascript')
     return Response('Not found', status=404)
 
 
