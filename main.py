3017c5c02f7b1247c4d5e63d69e6b2d0155c73e3
Adrian Dries <i@avdd.co>
2023-01-08 11:58:02 +1100

    refactor to load external script

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ from werkzeug.serving import run_simple
 def hello(rq: Request):  # pylint: disable=invalid-name
     if rq.path == '/':
         return Response(status=302, headers={'Location': '/home'})
+    if rq.path == '/app.js':
+        script = '''
+        const p = document.createElement('p');
+        p.textContent = 'Hello, world!'
+        p.className = 'Home';
+        document.body.appendChild(p);
+        '''
+        return Response(script, mimetype='text/javascript')
     if rq.path == '/home':
         html = '''
         <script>
         function StartLoading() {
-            setTimeout(AddHome, 100);
+            setTimeout(AddScript, 100);
         }
-        function AddHome() {
-            const p = document.createElement('p');
-            p.textContent = 'Hello, world!'
-            p.className = 'Home';
-            document.body.appendChild(p);
+        function AddScript() {
+            const script = document.createElement('script');
+            script.type = 'module';
+            script.src = './app.js';
+            document.body.appendChild(script);
         }
         </script>
         '''
