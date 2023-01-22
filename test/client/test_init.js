c6fda0ac3e1bbecad486a6e046c5ac9fedf4e0f9
Adrian Dries <i@avdd.co>
2023-01-22 20:43:35 +1100

    js test and deps

---
+++ test/client/test_init.js
@@ -0,0 +1,8 @@
+// @ts-check
+import { Start } from '../../src/loco/app.js';
+
+describe('app', function () {
+    it('works', function () {
+        expect(true).toBe(true);
+    })
+})
--- tools/dev-setup.sh
+++ tools/dev-setup.sh
@@ -10,3 +10,6 @@ $here/install-geckodriver.sh
 pip install -r ./python-requirements.txt
 python_user_site=$(python -m site --user-site)
 echo $top/src > $python_user_site/loco.pth
+. $NVM_DIR/nvm.sh
+nvm install --lts
+npm install
