ecdf051686785d758ce135f7afc7d8a02834ac6f
Adrian Dries <i@avdd.co>
2023-01-22 20:58:24 +1100

    failing test 'fetch css'

--- test/client/test_init.js
+++ test/client/test_init.js
@@ -1,8 +1,26 @@
 // @ts-check
-import { Start } from '../../src/loco/app.js';
+import { FetchCss } from '../../src/loco/app.js';
+
+function fetchMocker(data) {
+    return {
+        fetch(url, options) {
+            this.calledArgs = { url, options };
+            const rsp = {
+                async text() {
+                    return Promise.resolve(data);
+                }
+            }
+            return Promise.resolve(rsp);
+        }
+    }
+}
 
 describe('app', function () {
-    it('works', function () {
-        expect(true).toBe(true);
+    it('fetches css', async function () {
+        const testCss = 'css data';
+        const win = fetchMocker(testCss);
+        const css = await FetchCss(win, 'url');
+        expect(win.calledArgs.url).toBe('url');
+        expect(css).toBe(testCss);
     })
 })
